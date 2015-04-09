04bc6c831489dbc4f847ea9fee973cf04c6c7f84
Jordan <skorlir@gmail.com>
2015-04-09 02:37:42 -0500

    previousPage and nextPage animations working

--- app/scripts/formula_anim.js
+++ app/scripts/formula_anim.js
@@ -30,5 +30,50 @@ var formula_animator = (function () {
                  .removeClass('punched');
   }
 
+  _self.nextPage = function () {
+    var current_page = extend(qq('.page.current')[0])
+      , next_page    = extend(current_page.nextElementSibling);
+
+    if (!next_page || next_page.className.indexOf('page') === -1)
+      return;
+
+    current_page.addClass('prev');
+    next_page.addClass('next');
+
+    _self.formula.removeClass('persp').addClass('shuffle');
+    setTimeout(function () {
+      next_page.addClass('current');
+      current_page.removeClass('current');
+      setTimeout(function () {
+        next_page.removeClass('next');
+        current_page.removeClass('prev');
+        _self.formula.removeClass('shuffle');
+      }, 100);
+    }, 500);
+  }
+
+  _self.previousPage = function () {
+    var current_page = extend(qq('.page.current')[0])
+      , prev_page    = extend(current_page.previousElementSibling);
+
+    if (!prev_page || prev_page.className.indexOf('page') === -1)
+      return;
+
+    current_page.addClass('prev');
+    prev_page.addClass('next');
+
+    _self.formula.addClass('shuffle').addClass('reverse');
+    setTimeout(function () {
+      prev_page.addClass('current');
+      current_page.removeClass('current');
+      setTimeout(function () {
+        prev_page.removeClass('next');
+        current_page.removeClass('prev');
+        _self.formula.removeClass('shuffle')
+                     .removeClass('reverse');
+      }, 100);
+    }, 500);
+  }
+
   return _self;
 })();
